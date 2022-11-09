a2c917a0ac9f38d171c26905df41fb9c0fbc3f49
jitendra.sahoo <jitendra.sahoo@clevertap.com>
2022-11-09 16:46:41 +0530

    Android 13 push Updates

--- app/src/main/java/com/jitendract/jitdemo/MainActivity.java
+++ app/src/main/java/com/jitendract/jitdemo/MainActivity.java
@@ -1,7 +1,12 @@
 package com.jitendract.jitdemo;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.content.ContentValues.TAG;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
@@ -13,12 +18,17 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.clevertap.android.sdk.CleverTapAPI;
+import com.clevertap.android.sdk.Constants;
+import com.clevertap.android.sdk.PushPermissionResponseListener;
+import com.clevertap.android.sdk.inapp.CTLocalInApp;
 import com.clevertap.android.sdk.pushnotification.CTPushNotificationListener;
 import com.clevertap.android.sdk.pushnotification.PushConstants;
 
+import org.json.JSONObject;
+
 import java.util.HashMap;
 
-public class MainActivity extends AppCompatActivity implements CTPushNotificationListener {
+public class MainActivity extends AppCompatActivity implements CTPushNotificationListener, PushPermissionResponseListener {
 
     EditText identity,email,phone;
 
@@ -28,24 +38,66 @@ public class MainActivity extends AppCompatActivity implements CTPushNotificatio
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        CleverTapAPI.enableXiaomiPushOn(PushConstants.ALL_DEVICES);
 
+
+        CleverTapAPI.enableXiaomiPushOn(PushConstants.ALL_DEVICES);
         CleverTapAPI clevertapDefaultInstance = CleverTapAPI.getDefaultInstance(getApplicationContext());
         CleverTapAPI.setDebugLevel(CleverTapAPI.LogLevel.VERBOSE);
+        if (clevertapDefaultInstance != null) {
+            clevertapDefaultInstance.setPushPermissionNotificationResponseListener(this);
+        }
         clevertapDefaultInstance.setCTPushNotificationListener(this);
-        CleverTapAPI.createNotificationChannel(getApplicationContext(),"JitDemo","JitDemo","JitDemo", NotificationManager.IMPORTANCE_MAX,true);
-        CleverTapAPI.createNotificationChannel(getApplicationContext(),"r2d2","r2d2","r2d2 sound bad", NotificationManager.IMPORTANCE_MAX,true,"r2d2.mp3");
+
 
 
         identity = findViewById(R.id.identity);
         email = findViewById(R.id.email);
         phone = findViewById(R.id.phone);
 
+        @SuppressLint("RestrictedApi") JSONObject jsonObject = CTLocalInApp.builder()
+                .setInAppType(CTLocalInApp.InAppType.HALF_INTERSTITIAL)
+                .setTitleText("Get Latest Offers & Promotions")
+                .setMessageText("Click on allow to get the latest updates from us !!!!!")
+                .followDeviceOrientation(true)
+                .setPositiveBtnText("Let's Do THis!")
+                .setNegativeBtnText("Maybe Later.")
+                .setBackgroundColor(Constants.WHITE)
+                .setBtnBorderColor("#FFD700")
+                .setTitleTextColor(Constants.WHITE)
+                .setMessageTextColor(Constants.BLACK)
+                .setBtnTextColor(Constants.WHITE)
+                .setImageUrl("https://media-exp1.licdn.com/dms/image/C5622AQFo1izBws7Lhw/feedshare-shrink_2048_1536/0/1658384964241?e=2147483647&v=beta&t=lkJWbHD_8n5v27GtOg4gynsRu_PunE1Z33XY0jJetDQ")
+                .setBtnBackgroundColor(Constants.BLACK)
+                .build();
+//                .setInAppType(CTLocalInApp.InAppType.ALERT)
+//                .setTitleText("Get Notified")
+//                .setMessageText("Enable Notification permission")
+//                .followDeviceOrientation(true)
+//                .setPositiveBtnText("Allow")
+//                .setNegativeBtnText("Cancel")
+//                .build();
+        clevertapDefaultInstance.promptPushPrimer(jsonObject);
+//        clevertapDefaultInstance.promptForPushPermission(true);
 
+
+    }
+    @Override
+    public void onPushPermissionResponse(boolean accepted) {
+        CleverTapAPI clevertapDefaultInstance = CleverTapAPI.getDefaultInstance(getApplicationContext());
+        Log.i(TAG, "onPushPermissionResponse :  InApp---> response() called accepted="+accepted);
+        if(accepted){
+            //For Android 13+ we need to create notification channel after notification permission is accepted
+            CleverTapAPI.createNotificationChannel(getApplicationContext(),"JitDemo","JitDemo","JitDemo", NotificationManager.IMPORTANCE_MAX,true);
+            CleverTapAPI.createNotificationChannel(getApplicationContext(),"r2d2","r2d2","r2d2 sound bad", NotificationManager.IMPORTANCE_MAX,true,"r2d2.mp3");
+            clevertapDefaultInstance.pushEvent("Push Opted-in");
+        }
+        else{
+            clevertapDefaultInstance.pushEvent("Push Opted-out");
+        }
     }
 
 
-    public void loginClick(View view) {
+    public void loginClimck(View view) {
 
         String Email = email.getText().toString();
         String Identity = identity.getText().toString();
@@ -83,4 +135,5 @@ public class MainActivity extends AppCompatActivity implements CTPushNotificatio
     public void onNotificationClickedPayloadReceived(HashMap<String, Object> payload) {
         Toast.makeText(this, (CharSequence) payload,Toast.LENGTH_LONG).show();
     }
+
 }
--- app/src/main/java/com/jitendract/jitdemo/SplashScreen.java
+++ app/src/main/java/com/jitendract/jitdemo/SplashScreen.java
@@ -1,7 +1,11 @@
 package com.jitendract.jitdemo;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.app.NotificationManager;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -11,6 +15,7 @@ import android.view.WindowManager;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.clevertap.android.sdk.CleverTapAPI;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -28,6 +33,14 @@ public class SplashScreen extends AppCompatActivity {
         setContentView(R.layout.activity_splash_screen);
         imgView = findViewById(R.id.imgView);
         Glide.with(this).load(R.drawable.logogif).into(imgView);
+
+//        CleverTapAPI.createNotificationChannel(getApplicationContext(),"JitDemo","JitDemo","JitDemo", NotificationManager.IMPORTANCE_MAX,true);
+        
+//        Boolean val = permissonAccess();
+//        if (val){
+//            initClevertapGeofence();
+//        }
+//        else {}
 
         SharedPreferences prefs = getSharedPreferences("Login", MODE_PRIVATE);
         isLoggedIN =prefs.getBoolean("LoggedIn",false);
@@ -48,4 +61,39 @@ public class SplashScreen extends AppCompatActivity {
         }
 
     }
+
+//    private void initClevertapGeofence() {
+//    }
+
+//    private Boolean permissonAccess() {
+//        final Boolean[] val = new Boolean[1];
+//
+//        ActivityResultLauncher<String[]> locationPermissionRequest =
+//                registerForActivityResult(new ActivityResultContracts
+//                                .RequestMultiplePermissions(), result -> {
+//                            Boolean fineLocationGranted = result.getOrDefault(
+//                                    android.Manifest.permission.ACCESS_FINE_LOCATION, false);
+//                            Boolean coarseLocationGranted = result.getOrDefault(
+//                                    android.Manifest.permission.ACCESS_COARSE_LOCATION,false);
+//                            if (fineLocationGranted != null && fineLocationGranted) {
+//                                // Precise location access granted.
+//                                val[0] = true;
+//                            } else if (coarseLocationGranted != null && coarseLocationGranted) {
+//                                // Only approximate location access granted.
+//                                val[0] = true;
+//                            } else {
+//                                // No location access granted.
+//                                val[0] = false;
+//                            }
+//                        }
+//                );
+//
+//        locationPermissionRequest.launch(new String[] {
+//                android.Manifest.permission.ACCESS_FINE_LOCATION,
+//                Manifest.permission.ACCESS_COARSE_LOCATION
+//        });
+//
+//        return val[0];
+//
+//    }
 }
